b2478a77ccea069390f0c9c77eb4ffa36741bf67
Michael Halls-Moore <mike@quarkgluon.com>
2014-11-17 15:51:38 +0000

    Added FireOrder and BuildOrder

--- order.py
+++ order.py
@@ -12,3 +12,19 @@ class MovementOrder(Order):
             unit_key, "MOVEMENT"
         )
         self.new_position = new_position
+
+
+class FireOrder(Order):
+    def __init__(self, unit_key, firing_position):
+        super(FireOrder, self).__init__(
+            unit_key, "FIRE"
+        )
+        self.firing_position = firing_position
+
+
+class BuildOrder(Order):
+    def __init__(self, structure_key, build_unit_type):
+        super(BuildOrder, self).__init__(
+            structure_key, "BUILD"
+        )
+        self.build_unit_type = build_unit_type
